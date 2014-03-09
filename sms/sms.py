33ab7a7580a2b4e426946ad437c8f66759ba9d7f
Eric Anderle <eric@twilio.com>
2014-03-09 04:12:34 -0700

    grab latest sms xml

--- sms/sms.py
+++ sms/sms.py
@@ -1,6 +1,7 @@
 import dropbox
 import json
 import os
+from datetime import datetime
 
 def do_oauth():
     client_secrets = json.load(open('client_secrets.json'))
@@ -31,6 +32,30 @@ def read_token():
     token_file.close()
     return access_token
 
+def get_datetime(xml_file):
+    # Sun, 09 Mar 2014 09:04:54 +0000
+    return datetime.strptime(xml_file['modified'][:-6], '%a, %d %b %Y %H:%M:%S')
+
+def get_most_recent(client):
+    metadata = client.metadata('/Apps/SMSBackupRestore/')
+    xml_files = [x for x in metadata['contents'] if (x['mime_type'] == 'application/xml')]
+    return max(xml_files, key=get_datetime)
+
+def save_file(client, path):
+    f = open(os.path.expanduser('~/backup/sms/{}'.format(os.path.basename(path))), 'w')
+    f.write(client.get_file(path).read())
+    f.close()
+
 access_token = read_token() if os.path.isfile('token') else do_oauth()
 client = dropbox.client.DropboxClient(access_token)
-print 'linked account: ', client.account_info()
+
+print 'Finding most recent sms backup...'
+most_recent = get_most_recent(client)
+
+path = most_recent['path']
+basename = os.path.basename(path)
+print 'Found {}.'.format(basename)
+
+print 'Saving into ~/backup/sms/{}...'.format(basename)
+save_file(client, path)
+print 'Done!'
